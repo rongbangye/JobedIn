17419aeba9a24a3665152061468e65b21b308da2
Solomon <solomonmeresa8@gmail.com>
2020-11-01 00:33:33 -0700

    adds profile-page and profile-edit form

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -4,5 +4,9 @@ const router = require('express').Router();
 router.get('/profile', (req, res) => {
   res.render('profile');
 });
+// edit profile
+router.get('/profile/edit-profile', (req, res) => {
+  res.render('edit-profile');
+});
 
 module.exports = router;
