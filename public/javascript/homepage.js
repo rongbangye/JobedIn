e908ea51f64a071b2568df14c1ecde6222b7ed2f
Solomon <solomonmeresa8@gmail.com>
2020-11-05 03:22:30 -0800

    adds rendering from api

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -1,12 +1,13 @@
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const containerEl = document.querySelector('.job-container');
+const jobsEL = document.querySelector('.home-jobs');
 
 
 
 
 var searchJob = function(language,city){
-const apiURL = `http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${config.APP_ID}&app_key=${config.API_KEY}&results_per_page=20&what=${language}%20developer&where=${city}&content-type=application/json`;
+const apiURL = `http://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${config.APP_ID}&app_key=${config.API_KEY}&results_per_page=20&what=${language}%20developer&where=${city}&content-type=application/json`;
 
 
 fetch(proxyurl+ apiURL)
@@ -14,12 +15,32 @@ fetch(proxyurl+ apiURL)
     // console.log(response);
     return response.json()
 }).then(data =>{
-    console.log(data);
-    displayJob(data)
+    console.log(data.results[0].description);
+    console.log(data.results[0].location.display_name);
+    console.log(data.results[6]);
+    displayJob(data.results)
 })
 
 }
 var vew = function(){
-   searchJob("java","london")
+   searchJob("java","sanfrancisco")
 }
 vew();
+
+const displayJob = function(jobs){
+
+    let jobEL=document.createElement("ul");
+    // jobEL.addClass("m-2 p-3 border bg-light");
+   jobs.forEach(job=>{
+        
+        jobEL.innerHTML+= `
+        <li>
+        <div><h4>${job.title}</h4>
+        <p>${job.location.display_name}</p></div>
+        <div><p class="text-secondary">${job.description}</p>
+    </div></li>
+      `
+    });
+
+    jobsEL.appendChild(jobEL)
+}
