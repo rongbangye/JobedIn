95d95353e68f2a105beccbf2453274ff6c54a8c9
Solomon <solomonmeresa8@gmail.com>
2020-11-06 17:47:12 -0800

    adds edit-post and create-post sections

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -9,12 +9,59 @@ router.get('/', (req, res) => {
   res.render('profile');
 });
 // edit profile
-router.get('/edit-profile', (req, res) => {
+router.get('/edit-profile/:id', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect("/");
     return;
   }
   res.render('edit-profile');
+});
+// edit post
+router.get('/edit-post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_content',
+      'title',
+      'created_at'
+     ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('edit-post', {
+        post,
+        loggedIn: true
+       });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,32 @@
+async function editFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1];
+    
+    const title = document.querySelector('input[name="post-title"]').value;    
+    const post_content = document.querySelector('input[name="post-content"]').value;    
+    // const id = req.params.id;
+    
+    
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title:title,
+          post_content:post_content
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+    });
+        
+    if (response.ok) {
+        //   document.location.reload();
+          document.location.replace('/profile');
+    } else {
+          alert(response.statusText);
+    }
+  
+  }
+  
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
